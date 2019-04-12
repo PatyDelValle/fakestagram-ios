e5982c70d7cadddcd23742e88b1cf89b2a32f802
PatyDElValle <paty.del.valle@hotmail.com>
2019-04-11 22:06:29 -0500

    PostAuthorView, Avatar(sin foto) y label

--- fakestagram/Post/PostAuthorView.swift
+++ fakestagram/Post/PostAuthorView.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+//@IBDesignable permite visualizar en el StoryBoard sin compilar ni accesar a la vista
 
+@IBDesignable
 class PostAuthorView: UIView {
     var author: Author!
     let avatarView: SVGView = SVGView()
-    let label: UILabel = UILabel()
+    let nameLabel: UILabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,5 +26,34 @@ class PostAuthorView: UIView {
     }
     
     func setupView() {
+        nameLabel.backgroundColor = .cyan
+        addSubview(nameLabel)
+        
+        avatarView.clipsToBounds = true
+        avatarView.layer.cornerRadius = 26
+        avatarView.backgroundColor = .green
+        addSubview(avatarView)
+        
+        setupAutoLayout()
+    }
+
+    func setupAutoLayout () {
+        avatarView .translatesAutoresizingMaskIntoConstraints = false
+        //Lista de constrains que se activan a traves del metodo activate
+        NSLayoutConstraint.activate([
+            avatarView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+            avatarView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            avatarView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5),
+            avatarView.widthAnchor.constraint(equalToConstant: 52)
+            ])
+ 
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = "Paty Del Valle" //author.name
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: 15),
+            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5),
+            nameLabel.heightAnchor.constraint(equalToConstant: 32)
+            ])
     }
 }
